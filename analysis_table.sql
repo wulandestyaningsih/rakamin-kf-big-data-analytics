7fdce7e8251633c4ed0cbf9e7d5dc1fa197aec47
Wulan Destyaningsih <wulandestyaningsih@gmail.com>
2026-09-13 13:32:33 +0700

    Update analysis_table.sql

--- analysis_table.sql
+++ analysis_table.sql
@@ -17,9 +17,9 @@ SELECT
   -- Persentase gross laba berdasarkan harga obat (actual_price)
   CASE 
     WHEN p.price <= 50000 THEN 0.10
-    WHEN p.price > 50000 AND p.price <= 100000 THEN 0.15
-    WHEN p.price > 100000 AND p.price <= 300000 THEN 0.20
-    WHEN p.price > 300000 AND p.price <= 500000 THEN 0.25
+    WHEN p.price BETWEEN 50001 AND 100000 THEN 0.15
+    WHEN p.price BETWEEN 100001 AND 300000 THEN 0.20
+    WHEN p.price BETWEEN 300001 AND 500000 THEN 0.25
     ELSE 0.30
   END AS persentase_gross_laba,
 
@@ -30,9 +30,9 @@ SELECT
   (p.price * (1 - (t.discount_percentage / 100))) * 
   CASE 
     WHEN p.price <= 50000 THEN 0.10
-    WHEN p.price > 50000 AND p.price <= 100000 THEN 0.15
-    WHEN p.price > 100000 AND p.price <= 300000 THEN 0.20
-    WHEN p.price > 300000 AND p.price <= 500000 THEN 0.25
+    WHEN p.price BETWEEN 50001 AND 100000 THEN 0.15
+    WHEN p.price BETWEEN 100001 AND 300000 THEN 0.20
+    WHEN p.price BETWEEN 300001 AND 500000 THEN 0.25
     ELSE 0.30
   END AS nett_profit,
 
